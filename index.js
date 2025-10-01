2d3ad5a06938cdbfe29af7b02180cc53fa533d92
Rhodah Mulera <mulerarhodah@gmail.com>
2025-10-01 12:47:26 +0300

    understanding objects

--- index.js
+++ index.js
@@ -23,3 +23,43 @@ checkWinner(scoreDolphins, scoreKoalas);
 scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 checkWinner(scoreDolphins, scoreKoalas);
+
+
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    },
+};
+
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    },
+};
+
+// Calculate BMIs
+mark.calcBMI();
+john.calcBMI();
+
+// Compare and log result
+if (mark.bmi > john.bmi) {
+    console.log(
+        `${mark.fullName}'s BMI (${mark.bmi.toFixed(1)}) is higher than ${john.fullName}'s (${john.bmi.toFixed(1)})!`
+    );
+} else if (john.bmi > mark.bmi) {
+    console.log(
+        `${john.fullName}'s BMI (${john.bmi.toFixed(1)}) is higher than ${mark.fullName}'s (${mark.bmi.toFixed(1)})!`
+    );
+} else {
+    console.log(
+        `${mark.fullName} and ${john.fullName} have the same BMI (${mark.bmi.toFixed(1)})!`
+    );
+}
